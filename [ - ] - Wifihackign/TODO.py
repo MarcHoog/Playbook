b508e333c61c6d9e6c1a3efd2021f5eb9887b0ee
Marc <Marc.hoogendoorn@live.ln>
2021-04-14 14:40:07 +0200

    veel dingen

---
+++ [ - ] - Wifihackign/TODO.py
@@ -0,0 +1,8 @@
+# TODO Wifihacking
+#   [X]  fake beaconing
+#   [X]  Beacon scanning
+#   [ ]  Look at two tools not sure which one
+#           [ ] The one that fakes acces points i liked that one
+#           [ ] The one that cracks passwords over the wifi also cool one
+
+# NOTE: Everything together should be enough to fix wifi hacking in de BOK
